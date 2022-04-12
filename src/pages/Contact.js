848be3170988e457fbc076034de9eed145b83c68
gabe.mcmahon94@gmail.com <gabe.mcmahon94@gmail.com>
2022-04-11 20:57:51 -0400

    first commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from 'styled-components/macro';
 import { Button1 } from "../components/Button"
 import 'bootstrap/dist/css/bootstrap.min.css';
